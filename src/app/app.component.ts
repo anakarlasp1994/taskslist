7ac87a8a115d43a23fe44cdac9a04775c5cbafeb
anakarlasp1994 <anakarlasp1994@gmail.com>
2024-09-26 14:35:15 -0600

    Add to use task form and list component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from './task/task-form/task-form.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TaskFormComponent, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tasks-list';
+
+  numTasks: number;
+
+  loadList(event:number){
+    this.numTasks = event;
+  }
 }
